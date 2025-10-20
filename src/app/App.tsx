9e381757ff20a76e5ca9beb7850ad62f8b3faf54
Alex <cartnix21@gmail.com>
2025-10-20 18:42:47 +0300

    fea(shared): Secondary button ui created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import PrimaryButton from "../shared/ui/Buttons/PrimaryButton"
+import SecondaryButton from "../shared/ui/Buttons/SecondaryButton"
 
 function App() {
 
   return (
     <>
       <PrimaryButton />
+      <SecondaryButton />
     </>
   )
 }
---
+++ src/shared/ui/Buttons/SecondaryButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from 'antd';
+
+
+export default function SecondaryButton() {
+    return (
+        <>
+            <Button type="primary" size='large' className="
+            !bg-[#f2f2f2]
+            !text-[#1b1b1b]
+            hover:!bg-[#e6e6e6]
+            active:!bg-[#d9d9d9]
+            !border-none
+            !outline-none
+            !shadow-none
+            focus:!shadow-none
+            active:!shadow-none
+            focus:!outline-none
+            rounded-xl
+            transition-colors
+            duration-200
+        ">
+                Get Started
+            </Button>
+        </>
+    );
+};
