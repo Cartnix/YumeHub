3f2c0698a82cdf3a97f046e086475bd69043b782
Alex <cartnix21@gmail.com>
2025-10-30 18:09:24 +0300

    fea(style): Custom loader added

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,12 @@
-export default function HomeApp()
-{
-    return(
-        <div>
-            
-        </div>
+import { Container } from "../../shared/ui/Container";
+import Loader from "../../shared/ui/Loader/Loader";
+
+export default function HomeApp() {
+    return (
+        <main className="py-10 pt-[170px]">
+            <Container>
+                <Loader />
+            </Container>
+        </main>
     )
 }
--- src/widgets/AnimeCardsWrapper/ui/AnimeCardsGrid.tsx
+++ src/widgets/AnimeCardsWrapper/ui/AnimeCardsGrid.tsx
@@ -1,56 +1,45 @@
 import type { FC } from "react";
 import AnimeCard from "../../../entities/AnimeCard/ui/AnimeCard";
 import type { AnimeCardI } from "../../../entities/AnimeCard/model/AnimeCardI";
+import LoaderOverlay from "../../../shared/ui/Loader/LoaderOverlay";
 
 interface Props {
-  animes: AnimeCardI[];
-  isLoading: boolean;
-  hasMore: boolean;
-  showMore: () => void;
+    animes: AnimeCardI[];
+    isLoading: boolean;
+    hasMore: boolean;
+    showMore: () => void;
 }
 
 const AnimeCardsGrid: FC<Props> = ({ animes, isLoading, hasMore, showMore }) => {
-  if (isLoading) {
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {Array.from({ length: 8 }).map((_, i) => (
-          <div key={i} className="h-[350px] w-full rounded-2xl bg-zinc-800 animate-pulse" />
-        ))}
-      </div>
+        <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {animes.map((a) => (
+                    <AnimeCard
+                        key={a.title}
+                        title={a.title}
+                        year={a.year}
+                        desc={a.desc}
+                        background={a.background}
+                        genre={a.genre}
+                    />
+                ))}
+            </div>
+
+            {hasMore && (
+                <div className="flex justify-center mt-6">
+                    <button
+                        className="cursor-pointer w-full font-bold px-4 py-4 rounded-xl bg-[var(--color-offwhite)] text-[var(--color-dark-1)] hover:bg-[var(--color-btn-secondary-hover)] transition-colors"
+                        onClick={showMore}
+                    >
+                        Show more
+                    </button>
+                </div>
+            )}
+
+            {isLoading && <LoaderOverlay />}
+        </>
     );
-  }
-
-  if (!isLoading && animes.length === 0) {
-    return <div className="text-center text-[var(--color-gray-1)] py-8">No anime found.</div>;
-  }
-
-  return (
-    <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {animes.map((a) => (
-          <AnimeCard
-            key={a.title}
-            title={a.title}
-            year={a.year}
-            desc={a.desc}
-            background={a.background}
-            genre={a.genre}
-          />
-        ))}
-      </div>
-
-      {hasMore && (
-        <div className="flex justify-center mt-6">
-          <button
-            className="cursor-pointer w-full font-bold px-4 py-4 rounded-xl bg-[var(--color-offwhite)] text-[var(--color-dark-1)] hover:bg-[var(--color-btn-secondary-hover)] transition-colors"
-            onClick={showMore}
-          >
-            Show more
-          </button>
-        </div>
-      )}
-    </>
-  );
 };
 
 export default AnimeCardsGrid;
